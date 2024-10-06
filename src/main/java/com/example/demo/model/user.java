32b68f8ccaf5691a84464b679248edc311c5774c
shoyeb <2020BTECS00059@walchandsangli.ac.in>
2024-10-07 01:40:59 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/model/user.java
@@ -0,0 +1,13 @@
+package com.example.demo.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class user {
+    
+    @Generated()
+    int id;
+}
